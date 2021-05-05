1bae67f3ed47177c406d4a7d8e8cccfa6a27a0c5
x <x>
2021-05-05 13:15:25 +0200

    Agregado Login (Ejercicio3)

--- src/main/java/spring/Ejercicio1/controller/Ejercicio1Controller.java
+++ src/main/java/spring/Ejercicio1/controller/Ejercicio1Controller.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("")
@@ -23,5 +24,20 @@ public class Ejercicio1Controller {
 	public String despedida() {
 		return "contacto.html";
 	}
+	
+	@GetMapping("/login")
+	public String login(@RequestParam(required = true) String nombre, @RequestParam(defaultValue = "1234") String pass) {
+		if(validarPassword(nombre, pass)) {
+			return "saludo.html";
+		}else {
+			return "index.html";
+		}
+	}
+	
+	public boolean validarPassword(String nombre, String pass){
+		return true;
+	}
+	
+		
 	
 }
